e1011973e22f422ead44121548f94e2460e9e71f
Jon Led <jonledibarguren@gmail.com>
2025-10-20 22:35:52 +0200

    Add dotenv and cors

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { APP_PORT } from "./config/index.js";
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
