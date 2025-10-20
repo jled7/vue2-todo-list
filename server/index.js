288a0d67496907620bb4a0b05fa5d8f7bf68fe00
Jon Led <jonledibarguren@gmail.com>
2025-10-20 23:47:48 +0200

    Put everything together

--- server/index.js
+++ server/index.js
@@ -2,14 +2,33 @@ import express from "express";
 import cors from "cors";
 import { APP_PORT } from "./config/index.js";
 
+import addTaskUseCase from "./application/usecases/addTask.js";
+import listTasksUseCase from "./application/usecases/listTasks.js";
+import toggleTaskCompletionUseCase from "./application/usecases/toggleTaskCompletion.js";
+import EventBus from "./application/services/EventBus.js";
+
+import { MongoDBTaskRepository } from "./infrastructure/persistence/mongoDB/MongoDBTaskRepository.js";
+import ServerSideEvents from "./infrastructure/events/serverSideEvents.js";
+import buildTaskRoutes from "./infrastructure/http/taskRoutes.js";
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const eventBus = new EventBus();
+const repository = new MongoDBTaskRepository();
+const sse = new ServerSideEvents({ eventBus });
+
+app.use(
+  "/api/tasks",
+  buildTaskRoutes({
+    addTask: addTaskUseCase({ repository, eventBus }),
+    listTasks: listTasksUseCase({ repository }),
+    toggleTaskCompletion: toggleTaskCompletionUseCase({ repository, eventBus }),
+    sse,
+  })
+);
 
 app.listen(APP_PORT, () => {
   console.log(`Todo app backend listening on port ${APP_PORT}`);
