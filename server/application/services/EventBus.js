c6a7eb8b76c799adc3fd69b14acc0aa57df7bb13
Jon Led <jonledibarguren@gmail.com>
2025-10-20 23:45:44 +0200

    Add EventBus instead of relaying in SNS

---
+++ server/application/services/EventBus.js
@@ -0,0 +1,14 @@
+/* For a simple product as this todo list app, no reason to use SNS or other complex event bus system */
+
+export default class EventBus {
+  #handlers = new Map();
+  on(eventName, handler) {
+    const list = this.#handlers.get(eventName) || [];
+    list.push(handler);
+    this.#handlers.set(eventName, list);
+  }
+  emit(event) {
+    const list = this.#handlers.get(event.name) || [];
+    for (const h of list) h(event);
+  }
+}
